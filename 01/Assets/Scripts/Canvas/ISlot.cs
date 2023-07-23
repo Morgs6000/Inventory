21c5ed058c509c5cb04b1e5c48e29c4846eeb2d0
Morgana Stradivarius <55167884+Morgs6000@users.noreply.github.com>
2023-07-23 15:17:37 -0300

    Update Commit

---
+++ 01/Assets/Scripts/Canvas/ISlot.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ISlot : MonoBehaviour, IPointerDownHandler {
+    private GameObject dragging;
+
+    private IItem item;
+
+    [SerializeField] private GameObject itemPrefab;
+
+    private void Awake() {
+        dragging = GameObject.Find("Dragging Item");
+    }
+
+    private void Start() {
+        
+    }
+
+    private void Update() {
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        if(dragging.transform.childCount == 1) {
+            if(eventData.button == PointerEventData.InputButton.Left) {
+                if(transform.childCount == 0) {
+                    item = dragging.GetComponentInChildren<IItem>();
+                    item.getImage.raycastTarget = true;
+
+                    item.getParentAfterDrag = transform;
+                    item.transform.SetParent(item.getParentAfterDrag);
+                }
+            }
+            if(eventData.button == PointerEventData.InputButton.Right) {            
+                if(item.getStack < 2) {
+                    item.getImage.raycastTarget = true;
+
+                    item.getParentAfterDrag = transform;
+                    item.transform.SetParent(item.getParentAfterDrag);
+                }
+                else {
+                    GameObject itemObject = Instantiate(itemPrefab);
+
+                    IItem item2 = itemObject.GetComponentInChildren<IItem>();
+                    item2.InitialiseItem(item.getItem);
+
+                    itemObject.name = item2.getItemName;
+
+                    item2.transform.SetParent(transform);
+
+                    RectTransform rectTransform = item2.GetComponent<RectTransform>();
+                    rectTransform.localScale = transform.localScale;
+
+                    item.getStack--;
+                    item.RefreshCount();
+                }
+            }
+        }
+    }
+}
---
+++ 01/Assets/Scripts/Canvas/NotEnoughItems.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NotEnoughItems : MonoBehaviour {
+    private bool result;
+    [SerializeField] private IInventory iInventory;
+    [SerializeField] private Item[] itemsToPickup;
+
+    [SerializeField] private TextMeshProUGUI textMeshPro;
+
+    private void Start() {
+        
+    }
+
+    private void Update() {
+        
+    }
+
+    public void PickUpItem(int id) {
+        result = iInventory.AddItem(itemsToPickup[id]);
+
+        if(result) {
+            //Debug.Log("Item type added: " + voxelType);
+        }
+        else {
+            //Debug.Log("ITEM NOT ADDED");
+
+            WarningMensage();
+        }
+    }
+
+    private void WarningMensage() {
+        textMeshPro.text = "Inventario cheio";
+
+        ColorUtility.TryParseHtmlString("#FC5454", out Color color);
+        textMeshPro.color = color;
+
+        textMeshPro.gameObject.SetActive(true);
+
+        StartCoroutine(FadeOut());
+    }
+
+    private IEnumerator FadeOut() {
+        float delay = 1.0f;
+
+        yield return new WaitForSeconds(delay);
+
+        float fadeTime = 1.0f;
+        float elapsedTime = 0.0f;
+
+        Color startColor = textMeshPro.color;
+        Color endColor = new Color(startColor.r, startColor.g, startColor.b, 0f);
+
+        while (elapsedTime < fadeTime) {
+            float alpha = Mathf.Lerp(startColor.a, endColor.a, elapsedTime / fadeTime);
+            textMeshPro.color = new Color(startColor.r, startColor.g, startColor.b, alpha);
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        textMeshPro.color = endColor;
+
+        textMeshPro.gameObject.SetActive(false);
+    }
+}
