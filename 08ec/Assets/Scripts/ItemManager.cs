8930e5eac63bd2abfbd981ad5f96f4d6abfdbf70
Morgana Stradivarius <55167884+Morgs6000@users.noreply.github.com>
2023-08-16 15:09:16 -0300

    Update Commit

---
+++ 08ec/Assets/Scripts/ItemManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour {
+    public Texture itemAtlas;
+    public Texture voxelAtlas;
+    public Texture renderTexture;
+
+    public static List<ItemData> items = new List<ItemData>();
+
+    /*
+    private void Start() {
+        StartCoroutine(Teste());
+    }
+    */
+
+    private void Start() {
+    //private IEnumerator Teste() {
+        //items.Add(VoxelManager.voxels);
+        //foreach(VoxelData voxelData in VoxelManager.voxels) {
+        for(int i = 0; i < VoxelManager.voxels.Count; i++) {
+            VoxelData voxelData = VoxelManager.voxels[i];
+            /*
+            ItemData newItem = new ItemData(
+                "teste",
+                new Vector2(11, 2)
+            );
+            */
+            //*
+            ItemData newItem = new ItemData(
+                voxelData.voxelID,
+                voxelAtlas,
+                voxelData.itemUV
+            );
+            //*/
+            /*
+            ItemData newItem = new ItemData(
+                voxelData.voxelID,
+                renderTexture,
+                false
+            );
+            */
+
+            items.Add(newItem);
+        }
+
+        items.Add(new ItemData(
+            "iron_pickaxe",
+            itemAtlas,
+            new Vector2(2, 6)
+        ));
+
+        items.Add(new ItemData(
+            "apple",
+            itemAtlas,
+            new Vector2(10, 0)
+        ));
+        
+        items.Add(new ItemData(
+            "coal",
+            itemAtlas,
+            new Vector2(7, 0)
+        ));
+
+        //yield return null;
+    }
+}
